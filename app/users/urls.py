8cc9880417e63aa64923e825622eff0f0cfe283b
Kuczu <pawelkucz93@gmail.com>
2017-05-01 22:44:46 +0200

    changed login && registration mechanism

--- app/users/urls.py
+++ app/users/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'user'  # for template namespace
+
 urlpatterns = [
-    url(r'^register/$', views.register, name="registration"),
-    url(r'^login/$', views.login_page, name="login"),
-    url(r'^logintest/$', views.logintest, name="login"),
+    url(r'^guest/$', views.welcome, name="welcome"),
+    url(r'^guest/register/$', views.register, name="registration"),
+    url(r'^guest/login/$', views.login_page, name="login"),
+    url(r'^$', views.logintest, name="logged"),
 ]
--- app/users/views.py
+++ app/users/views.py
@@ -1,9 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from .forms import *
 
 
+def welcome(request, register_form=None, login_form=None):
+    if request.user.is_authenticated():
+        return redirect('user:logged')
+
+    if register_form is None:
+        register_form = RegistrationForm()
+
+    if login_form is None:
+        login_form = LoginForm()
+
+    return render(request, "not_logged_welcome.html", {'login_form': login_form, 'register_form': register_form})
+
+
 def register(request):
+    if request.user.is_authenticated():
+        return redirect('user:logged')
+
     if request.method == "POST":
         form = RegistrationForm(request.POST)
 
@@ -11,14 +27,19 @@ def register(request):
             form.save()
 
             return render(request, "registered.html")
-    else:
-        form = RegistrationForm()
-        return render(request, "registration_form.html", {'form': form})
 
-    return render(request, "registration_form.html", {'form': form})
+        return welcome(request, register_form=form)
+
+    return redirect('user:welcome')
 
 
 def login_page(request):
+    if request.user.is_authenticated():
+        return redirect('user:logged')
+
+    if request.user.is_authenticated():
+        redirect('user:logged')
+
     if request.method == "POST":
         form = LoginForm(request.POST)
 
@@ -26,12 +47,11 @@ def login_page(request):
             user = authenticate(login=form.cleaned_data['login'], password=form.cleaned_data['password'])
             login(request, user)
 
-            return render(request, "logged.html")
+            return redirect("user:logged")
 
-        return render(request, "login.html", {'form': form})
-    else:
-        form = LoginForm()
-        return render(request, "login.html", {'form': form})
+        return welcome(request, login_form=form)
+
+    return redirect('user:welcome')
 
 
 def logintest(request):
